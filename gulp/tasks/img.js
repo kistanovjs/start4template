d8e50e6621a6efb6011fa2d37faa1c4fef676dff
Kistanov <rizzydizzy@mail.ru>
2020-07-20 12:58:20 +0300

    фильтр в диапазоне

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,11 +1,11 @@
 module.exports = function() {
     $.gulp.task('img:dev', () => {
-        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif,svg}')
             .pipe($.gulp.dest('./build/app/img/'));
     });
 
     $.gulp.task('img:build', () => {
-        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif,svg}')
             .pipe($.gp.tinypng(P_6pt-cH6u3kfe0DZ_LYjtl04KsqPpzw))
             .pipe($.gulp.dest('./build/app/img/'));
     });
